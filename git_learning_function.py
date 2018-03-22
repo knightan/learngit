566dc82e9834c6b94faf02665a3cedfb1f6670f6
knightan <wasunshine@live.com>
2018-03-22 21:43:56 +0800

    hanoti

--- git_learning_function.py
+++ git_learning_function.py
@@ -20,5 +20,13 @@ def product(*nums):
         n = n + 1
     return production
 
+def hanoti(n,x1,x2,x3):
+    if(n == 1):
+        print('move:',x1,'-->',x3)
+        return
+    hanoti(n-1,x1,x3,x2)
+    print('move:',x1,'-->',x3)
+    hanoti(n-1,x2,x1,x3)
+hanoti(3,'A','B','C')       
 a = product(2,3)
 print(a)

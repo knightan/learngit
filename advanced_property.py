6aa67137f46b367b7d36e04e217dd91540e68cc7
knightan <wasunshine@live.com>
2018-03-24 21:13:56 +0800

    list comprehensions

--- advanced_property.py
+++ advanced_property.py
@@ -65,3 +65,12 @@ else:
     print('测试成功!')
 
 #print(findMinAndMax(L1))
+    
+#List Comprehensions
+L1 = ['Hello', 'World', 18, 'Apple', None]    
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')

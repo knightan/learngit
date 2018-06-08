868c42c994419a1db4a926c0e6be4e54ac1c1709
knightan <wasunshine@live.com>
2018-06-08 20:08:49 +0800

    I forget what I have done

--- ad_oop.py
+++ ad_oop.py
@@ -23,34 +23,66 @@ __author__ = 'Arthur Wang'
 #print(g.score)
 
 #使用@property
-class Screen(object):
+#class Screen(object):
+#
+#    @property
+#    def width(self):
+#        return self._width
+#
+#    @width.setter
+#    def width(self, value):
+#        self._width = value
+#        
+#    @property
+#    def height(self):
+#        return self._height
+#
+#    @height.setter
+#    def height(self, value):
+#        self._height = value
+#        
+#    @property
+#    def resolution(self):
+#        return self._width*self._height
+#    
+## 测试:
+#s = Screen()
+#s.width = 1024
+#s.height = 768
+#print('resolution =', s.resolution)
+#if s.resolution == 786432:
+#    print('测试通过!')
+#else:
+#    print('测试失败!')
 
-    @property
-    def width(self):
-        return self._width
+#定制类
 
-    @width.setter
-    def width(self, value):
-        self._width = value
-        
-    @property
-    def height(self):
-        return self._height
+#__str__
+#class Student(object):
+#    def __init__(self, name):
+#         self.name = name
+#    def __str__(self):
+#         return 'Student object (name: %s)' % self.name
+#    __repr__ = __str__
+#    
+#print(Student('Michael'))
+#__iter__
+#class Fib(object):
+#    def __init__(self):
+#        self.a, self.b = 0, 1 # 初始化两个计数器a，b
+#
+#    def __iter__(self):
+#        return self # 实例本身就是迭代对象，故返回自己
+#
+#    def __next__(self):
+#        self.a, self.b = self.b, self.a + self.b # 计算下一个值
+#        if self.a > 100000: # 退出循环的条件
+#            raise StopIteration()
+#        return self.a # 返回下一个值
+#
+#for n in Fib():
+#    print(n)
 
-    @height.setter
-    def height(self, value):
-        self._height = value
-        
-    @property
-    def resolution(self):
-        return self._width*self._height
-    
-# 测试:
-s = Screen()
-s.width = 1024
-s.height = 768
-print('resolution =', s.resolution)
-if s.resolution == 786432:
-    print('测试通过!')
-else:
-    print('测试失败!')
+#__getitem__
+print(range(100))
+
